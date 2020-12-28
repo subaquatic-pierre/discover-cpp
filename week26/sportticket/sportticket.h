48107bd8c8d68e27f391c638e65f4a8942c95ad7
Pierre <subaquatic.pierre@gmail.com>
2020-12-28 21:36:05 +0400

    Completed week26 sportticket lab

---
+++ week26/sportticket/sportticket.h
@@ -0,0 +1,36 @@
+// #include "../showtickets/showticket.h"
+// #include <iostream>
+#include "showticket.h"
+
+class SportTicket : public ShowTicket
+{
+private:
+    bool beerSold;
+
+public:
+    SportTicket(string row, string seat) : ShowTicket(row, seat)
+    {
+        this->beerSold = false;
+    }
+
+    void sell_beer()
+    {
+        this->beerSold = true;
+    }
+
+    bool beer_sold()
+    {
+        return this->beerSold;
+    }
+    string print_ticket()
+    {
+        string row = ShowTicket::getRowNum();
+        string seat = ShowTicket::getSeatNum();
+        string seatSold = ShowTicket::is_sold() ? "sold" : "available";
+        string beerSold = this->beer_sold() ? "beer" : "nobeer";
+        // string message = "BEER: \nRow: " + row + "\nSeat: " + seat + "\nIs sold: " + seatSold + "\nBeer Sold: " + beerSold;
+        string message = row + " " + seat + " " + seatSold + " " + beerSold;
+        // cout << message << endl;
+        return message;
+    }
+};
